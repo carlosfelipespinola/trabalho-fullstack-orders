91a76757c39e59a2e55caaa5ebc18e6720ffe1f4
Carlos Felipe <carlosfelipespiinola@gmail.com>
2022-09-01 22:12:27 -0300

    Altera texto de documentação da api

--- src/main/java/dev/carlosfelipe/pedido/sanduicheria/orders/application/controller/OrdersErrorDocumentationController.java
+++ src/main/java/dev/carlosfelipe/pedido/sanduicheria/orders/application/controller/OrdersErrorDocumentationController.java
@@ -10,7 +10,7 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 public class OrdersErrorDocumentationController {
   @GetMapping("/errors")
-  @ApiOperation(value = "Retorna uma lista com todos os erros que a api pode retornar. Esse chamada está aqui apenas para documentação")
+  @ApiOperation(value = "Retorna uma lista com TODOS os erros que a api pode retornar")
   Collection<OrderControllerException> errors() {
       return OrdersExceptionHandler.exceptionErrorMapping.values();
   }
