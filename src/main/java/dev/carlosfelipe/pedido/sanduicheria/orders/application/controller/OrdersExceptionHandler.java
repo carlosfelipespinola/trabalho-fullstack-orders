ef103b6dc7a8ca32b771fefe5f452094bf22c8f3
Carlos Felipe <carlosfelipespiinola@gmail.com>
2022-09-01 21:57:38 -0300

    Remove import não usado

--- src/main/java/dev/carlosfelipe/pedido/sanduicheria/orders/application/controller/OrdersExceptionHandler.java
+++ src/main/java/dev/carlosfelipe/pedido/sanduicheria/orders/application/controller/OrdersExceptionHandler.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
